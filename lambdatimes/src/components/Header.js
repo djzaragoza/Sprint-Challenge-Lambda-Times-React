8449d8f53299a2bec5503838743adc0929020901
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-15 11:26:41 -0700

    working on header.js file

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+
+const Container = styled.div `
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`
+
+
 
 const Header = () => {
   return (
