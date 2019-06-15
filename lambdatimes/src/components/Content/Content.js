12701b37c677e7a5cc540583b427eab8c1397a05
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-15 10:40:56 -0700

    working on content.js file carousel feature

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from './Carousel/Carousel';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -18,6 +19,10 @@ export default class Content extends Component {
 
   componentDidMount() {
     // Once the component has mounted, get the data and reflect that data on the state.
+    this.setState({
+      tabs:tabData,
+      cards: cardData, 
+    })
   }
 
   changeSelected = tab => {
