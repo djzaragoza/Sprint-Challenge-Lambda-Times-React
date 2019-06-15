31b01bf2eb29874261d84e0adbcd2a627c0230a2
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-15 11:31:46 -0700

    debugging header.js mostly finished

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -15,9 +15,39 @@ const Container = styled.div `
   height: 120px;
   margin-top: 44px;
   padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
 `
 
+const Date = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 25px;
+  flex: 1;
+`
 
+const Temp = styled.span `
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`
+
+const Title = styled.h1 `
+  font-size: 60px;
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+` 
 
 const Header = () => {
   return (
